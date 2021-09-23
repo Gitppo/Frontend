e92de6045c7103bd4e489963e2846cfd7424ed0c
sunung007@hanyang.ac.kr <KellanLukas1103!>
2021-09-23 17:24:35 +0900

    Home : 버튼 스크롤 이벤트

--- src/domain/Home/index.js
+++ src/domain/Home/index.js
@@ -1,10 +1,33 @@
 import Button from "../../components/Button";
 import "./style.css";
+import {useState} from "react";
+
+const btnMainColor = "#002d84";
+const remToPx = (rem) =>
+  rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 
 function Home() {
+  const [rem4, rem6, rem8] = [remToPx(4), remToPx(6), remToPx(8)];
+  const [btnStyle, setBtnStyle] = useState({
+    marginTop: `${rem6}px`,
+    background: btnMainColor,
+  });
+
+  const onHomeScroll = (e) => {
+    const scrolled = e.target.scrollTop;
+    if (scrolled >= rem8)
+      setBtnStyle({...btnStyle, background: "white", color: btnMainColor});
+    else if (scrolled < rem8)
+      setBtnStyle({...btnStyle, background: btnMainColor, color: "white"});
+    if (scrolled < rem4)
+      setBtnStyle({...btnStyle, marginTop: `${rem6 - scrolled}px`});
+  };
+
   return (
-    <div className={"home"}>
-      <Button className={"home-login-button"}>깃 헙 로그인</Button>
+    <div className={"home"} onScroll={onHomeScroll}>
+      <Button className={"home-login-button"} style={btnStyle}>
+        깃 헙 로그인
+      </Button>
 
       <div className={"home-intro-wrapper"}>
         <div className={"home-intro-card"} id={"left"}>
