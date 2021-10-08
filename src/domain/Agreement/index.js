146d7c21b74b2ed10c8e15e178e8fc2bfc3c7fe1
sunung007@hanyang.ac.kr <KellanLukas1103!>
2021-10-08 14:05:26 +0900

    - 페이지 별 헤더 색상 변경 - 파란색 radio button component 생성 - agree 페이지 타이틀 플래그 아이콘 추가 - 여러 img 파일 추가 (in Public/icon/...)

--- src/domain/Agreement/index.js
+++ src/domain/Agreement/index.js
@@ -1,4 +1,5 @@
 import RoundContainer from "../../components/App/RoundContainer";
+import RadioBtn from "../../components/RadioBtn";
 import "./style.css";
 
 const contract = [
@@ -9,31 +10,51 @@ const contract = [
 function Agreement() {
   return (
     <div className={"agree"}>
-      <h2 className={"beautiful-title"}>이용 약관 동의</h2>
+      <div className={"agree-title"}>
+        <img id={"agree-title-flag"} src={"/icon/flag.png"} alt={"flag icon"} />
+        <h2 className={"beautiful-title"}>이용 약관 동의</h2>
+      </div>
+
       <RoundContainer>
-        <div>
+        <div className={"agree-contract-title"}>
           <h2 className={"beautiful-title"}>
             <div>이용약관, 개인정보 수집 및 이용, 프로모션 알림,</div>
             <div>메일 및 푸시 알림 수신(선택)에 모두 동의합니다.</div>
           </h2>
+          <RadioBtn />
         </div>
+
+        {/* 구분선 */}
         <hr />
-        <h4>만 14세 이상입니다.</h4>
 
+        {/* 동의 1 */}
         <div className={"agree-contract-title"}>
-          <h4>깃포 이용약관 동의</h4>
-        </div>
-        <div className={"agree-contract-content"}>
-          <h4>이용약관</h4>
-          <div>{contract[0]}</div>
+          <h4>만 14세 이상입니다.</h4>
+          <RadioBtn />
         </div>
 
-        <div className={"agree-contract-title"}>
-          <h4>깃포 이용약관 동의</h4>
+        {/* 동의 2 */}
+        <div>
+          <div className={"agree-contract-title"}>
+            <h4>깃포 이용약관 동의</h4>
+            <RadioBtn />
+          </div>
+          <div className={"agree-contract-content"}>
+            <h4>이용약관</h4>
+            <div>{contract[0]}</div>
+          </div>
         </div>
-        <div className={"agree-contract-content"}>
-          <h4>이용약관</h4>
-          <div>{contract[1]}</div>
+
+        {/* 동의 3 */}
+        <div>
+          <div className={"agree-contract-title"}>
+            <h4>깃포 이용약관 동의</h4>
+            <RadioBtn />
+          </div>
+          <div className={"agree-contract-content"}>
+            <h4>이용약관</h4>
+            <div>{contract[1]}</div>
+          </div>
         </div>
 
         <div className={"agree-button-container"}>
