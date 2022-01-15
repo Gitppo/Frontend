34eb65542cb6e22fc52a7f6b2c4adc884eb56b96
catherine <68558189+jg980404@users.noreply.github.com>
2022-01-15 22:03:09 +0900

    mypage, gitrepodetail 페이지 수정 - 변수 넣기 - gitrepodetail 변경된css 반영

--- src/domain/Mypage/index.js
+++ src/domain/Mypage/index.js
@@ -1,6 +1,22 @@
 import RoundContainer from "../../components/RoundContainer";
 import "./style.css";
 
+const data = {
+  num: {
+    savedPortfolio: "3",
+    finishedPortfolio: "1",
+    originPortfolio: "2"
+  },
+  portfolioBox: {
+    title: "2021 조깃포 LG 포트폴리오",
+    creation:"2021.04.04",
+    revision: {
+      date: "2021.05.05",
+      time: "19:00"
+    }
+  }
+}
+
 function Mypage() {
   return (
     <div>
@@ -13,31 +29,31 @@ function Mypage() {
         <div className={"mypage-upper-box-right"}>
           <div className={"mypage-manage"}>
             <span>임시 저장 중인 포트폴리오</span>
-            <span className={"beautiful-title"}>1</span>
+            <span className={"beautiful-title"}>{data.num.savedPortfolio}</span>
           </div>
           <div className={"mypage-manage"}>
             <span>최종 완성 포트폴리오</span>
-            <span className={"beautiful-title"}>3</span>
+            <span className={"beautiful-title"}>{data.num.finishedPortfolio}</span>
           </div>
         </div>
       </div>
 
       <RoundContainer>
-        <h1 className={"beautiful-title"}>기존 포트폴리오 (3)</h1>
+        <h1 className={"beautiful-title"}>기존 포트폴리오 ({data.num.originPortfolio})</h1>
 
         <ul className={"mypage-wrapper-box"}>
           <li>
             <h4 className={"mypage-wrapper-box-title"}>
-              2021 조깃포 삼성 포트폴리오
+              {data.portfolioBox.title}
             </h4>
-            <div className={"mypage-wrapper-box-date"}>생성 2021.09.09</div>
+            <div className={"mypage-wrapper-box-date"}>생성 {data.portfolioBox.creation}</div>
             <div className={"mypage-wrapper-box-date"}>
-              수정 2021.09.09 19:00
+              수정 {data.portfolioBox.revision.date} {data.portfolioBox.revision.time}
             </div>
             <div className={"mypage-wrapper-box-button"}>
               <button className={"mypage-wrapper-box-button-left round-button"}>
                 수정
-              </button>
+              </button>        
               <button className={"round-button"}>삭제</button>
             </div>
           </li>
@@ -62,5 +78,5 @@ function Mypage() {
     </div>
   );
 }
-
+ 
 export default Mypage;
