217ac390f191039b9855e9f00cf2cb562c655767
sunung007@hanyang.ac.kr <KellanLukas1103!>
2022-02-14 15:04:32 +0900

    애니메이션/이미지 추가 - 로딩 페이지 : 애니메이션 추가 - 에러 페이지 : 이미지 추가 - 헤더 : 마이페이지 경로 수정 - App.css : container 최소 높이 설정

--- src/domain/Error/Error404.js
+++ src/domain/Error/Error404.js
@@ -1,4 +1,5 @@
 import {useLocation} from "react-router";
+import errorHippo from "../../assets/hippo-transparent.png";
 
 function Error404() {
   const location = useLocation();
@@ -8,6 +9,10 @@ function Error404() {
         <div className={"large-text-front"}>
           <div>404</div>
           <div>NOT FOUND</div>
+        </div>
+
+        <div className="error-hippo">
+          <img src={errorHippo} alt="" style={{transform: "scaleX(-1)"}} />
         </div>
       </>
     );
--- src/domain/Error/ErrorLoading.js
+++ src/domain/Error/ErrorLoading.js
@@ -1,7 +1,16 @@
+import errorHippo from "../../assets/hippo-transparent.png";
+
 function ErrorLoading() {
   return (
     <>
-      <div className={"large-text-front"}>로딩-실패</div>
+      <div className={"large-text-front"}>
+        로딩-실패
+        <br />
+        ㅠ_ㅠ
+      </div>
+      <div className="error-hippo">
+        <img src={errorHippo} alt="" />
+      </div>
     </>
   );
 }
